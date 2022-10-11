96f53d827ed203230edb44dbf39e82e946aa8520
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2022-10-11 10:13:30 +0300

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <div>
+        <article>
+            <h1><a href="/post">blogPost 1</a></h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae perspiciatis debitis minima facere vero quidem nesciunt labore, aspernatur obcaecati unde consectetur. Alias debitis expedita totam exercitationem obcaecati labore ex aperiam.</P>
+        </article>
+    </div>
+
+    <div class="border">
+        <article>
+            <h1><a href="/post">blogPost 2</a></h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae perspiciatis debitis minima facere vero quidem nesciunt labore, aspernatur obcaecati unde consectetur. Alias debitis expedita totam exercitationem obcaecati labore ex aperiam.</P>
+        </article>
+    </div>
+
+    <div class="border">
+        <article>
+            <h1><a href="/post">blogPost 3</a></h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae perspiciatis debitis minima facere vero quidem nesciunt labore, aspernatur obcaecati unde consectetur. Alias debitis expedita totam exercitationem obcaecati labore ex aperiam.</P>
+        </article>
+    </div>
+</body>
+</html>
